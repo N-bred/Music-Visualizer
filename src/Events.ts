273f1b3363353d246a5c601ac302748ff32de43d
Nbred <thehardyboiz1@gmail.com>
2025-07-11 12:14:18 -0500

    Removed unused events

--- src/Events.ts
+++ src/Events.ts
@@ -5,32 +5,15 @@ export const changedSongStateName = "changedSongState";
 export const changedSongStateEvent = new CustomEvent(changedSongStateName);
 
 export const newSongSelectedName = "newSongSelected";
-
 export const changedVolumeName = "changedVolume";
-
 export const songUploadedName = "songUploaded";
-
 export const changedSongIndexName = "changedSongIndex";
-export const changedSongIndexEvent = new CustomEvent(changedSongIndexName);
-
 export const changedThemeIndexName = "changedThemeIndex";
-export const changedThemeIndexEvent = new CustomEvent(changedThemeIndexName);
-
 export const changedSceneIndexName = "changedSceneIndex";
-export const changedSceneIndexEvent = new CustomEvent(changedSceneIndexName);
-
 export const changedRotationCheckboxName = "changedRotationCheckbox";
-export const changedRotationCheckboxEvent = new CustomEvent(changedRotationCheckboxName);
-
 export const changedPanCheckboxName = "changedPanCheckbox";
-export const changedPanCheckboxEvent = new CustomEvent(changedPanCheckboxName);
-
 export const changedZoomCheckboxName = "changedZoomCheckbox";
-export const changedZoomCheckboxEvent = new CustomEvent(changedZoomCheckboxName);
-
 export const AddedNewThemeName = "AddedNewTheme";
-export const AddedNewThemeEvent = new CustomEvent(AddedNewThemeName);
-
 export const progressBarClickedName = "progressBarClicked";
 
 export const songEndedName = "songEnded";
