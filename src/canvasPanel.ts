e9777e9b27864afee7988381482ad77e19e45bc5
Nbred <thehardyboiz1@gmail.com>
2025-07-17 18:46:08 -0500

    Adjusted styles for  theater mode and fullscreen mode along with their respective functionality

--- src/canvasPanel.ts
+++ src/canvasPanel.ts
@@ -3,29 +3,67 @@ export default class CanvasPanel {
   private fullscreenButton: HTMLButtonElement;
   private canvas: HTMLCanvasElement;
   private mode: "regular" | "fullscreen" | "theater";
+  private app: HTMLDivElement;
 
   constructor(canvas: HTMLCanvasElement) {
     this.theaterButton = document.querySelector("#canvas-theater-button")!;
     this.fullscreenButton = document.querySelector("#canvas-fullscreen-button")!;
     this.canvas = canvas;
     this.mode = "regular";
+    this.app = document.querySelector(".app")!;
 
     // EVENTS
     this.theaterButton.addEventListener("click", () => this.handleTheaterButton());
     this.fullscreenButton.addEventListener("click", () => this.handleFullscreenButton());
   }
 
+  handleAddTheaterMode() {
+    this.theaterButton.classList.add("active");
+    this.app.classList.add("theater");
+  }
+
+  handleRemoveTheaterMode() {
+    this.theaterButton.classList.remove("active");
+    this.app.classList.remove("theater");
+  }
+
+  handleAddFullscreen() {
+    this.fullscreenButton.classList.add("active");
+    this.canvas.parentElement!.requestFullscreen();
+  }
+
+  handleRemoveFullscreen() {
+    this.fullscreenButton.classList.remove("active");
+    document.exitFullscreen();
+  }
+
   handleTheaterButton() {
-    console.log(this.canvas);
+    if (this.mode === "theater") {
+      this.handleRemoveTheaterMode();
+      this.mode = "regular";
+    } else if (this.mode === "fullscreen") {
+      this.handleRemoveFullscreen();
+      this.handleAddTheaterMode();
+      this.mode = "theater";
+    } else if (this.mode === "regular") {
+      this.handleAddTheaterMode();
+      this.mode = "theater";
+    }
+    window.dispatchEvent(new Event("resize"));
   }
 
   handleFullscreenButton() {
+    this.handleRemoveTheaterMode();
     if (this.mode === "fullscreen") {
+      this.handleRemoveFullscreen();
       this.mode = "regular";
-      document.exitFullscreen();
-    } else {
+    } else if (this.mode === "theater") {
+      this.handleRemoveTheaterMode();
+      this.handleAddFullscreen();
       this.mode = "fullscreen";
-      this.canvas.parentElement!.requestFullscreen();
+    } else {
+      this.handleAddFullscreen();
+      this.mode = "fullscreen";
     }
   }
 }
--- src/style.ts
+++ src/style.ts
@@ -1,42 +1,28 @@
 import Player from "./player";
 
-
 const panelSongs: HTMLDivElement = document.querySelector(".panel-songs")!;
 const panelData: HTMLDivElement = document.querySelector(".panel-data")!;
 
-const panelSongsButton: HTMLButtonElement = document.querySelector(
-  "#hamburger-button-panel-songs"
-)!;
-const panelDataButton: HTMLButtonElement = document.querySelector(
-  "#hamburger-button-panel-data"
-)!;
+const panelSongsButton: HTMLButtonElement = document.querySelector("#hamburger-button-panel-songs")!;
+const panelDataButton: HTMLButtonElement = document.querySelector("#hamburger-button-panel-data")!;
 
 const menuState = {
   currentElement: panelSongs,
   isActive: false,
 };
 
-const handleButtonClick = (
-  button: HTMLButtonElement,
-  panel: HTMLDivElement
-) => {
+const handleButtonClick = (button: HTMLButtonElement, panel: HTMLDivElement) => {
   button.addEventListener("click", (e: Event) => {
     e.preventDefault();
 
     if (menuState.isActive) {
       menuState.currentElement.classList.remove("active");
       menuState.isActive = false;
-
-      if (menuState.currentElement !== panel) {
-        menuState.currentElement = panel;
-        menuState.isActive = true;
-        panel.classList.add("active");
-      }
-    } else {
-      menuState.currentElement = panel;
-      menuState.isActive = true;
-      panel.classList.add("active");
     }
+
+    menuState.currentElement = panel;
+    menuState.isActive = true;
+    panel.classList.add("active");
   });
 };
 
