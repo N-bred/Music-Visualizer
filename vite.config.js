934a80fe68e5bbfebe4fbe9685bd1844a7a5ce1d
Nbred <thehardyboiz1@gmail.com>
2025-07-22 08:21:00 -0500

    Adjusted new configuration on vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,9 @@
-export default {
-  base: "/music-visualizer",
+import { defineConfig, loadEnv } from "vite";
+
+export default ({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+  const base = mode === "development" ? "/" : "/music-visualizer/";
+  return defineConfig({
+    base,
+  });
 };
